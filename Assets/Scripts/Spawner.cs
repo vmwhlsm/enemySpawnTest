ea9a80182cd11bb76a803e58dc3d72020ba0fa15
Vladislav Petrov <vth.allmighty@gmail.com>
2022-07-25 23:28:04 +0300

    Refactor

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(GameObject))]
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _spawnPointParent;
+    [SerializeField] private Enemy _enemy;
+
+    private System.Random _random = new System.Random();
+    private int _randomMin = 0;
+    private int _randomMax;
+    private Transform[] _spawnPoints;
+    private float _interval = 2;
+    private bool _spawnIsNeeded = true;
+
+    private void Start()
+    {
+        _spawnPoints = _spawnPointParent.GetComponentsInChildren<Transform>();
+        _randomMax = _spawnPoints.Length - 1;
+        StartCoroutine(SpawnEnemy());
+    }
+
+    private IEnumerator SpawnEnemy()
+    {
+        while (_spawnIsNeeded)
+        {
+            int randomIndex = _random.Next(_randomMin, _randomMax);
+            Instantiate(_enemy.gameObject, _spawnPoints[randomIndex]);
+
+            yield return new WaitForSeconds(_interval);
+        }
+    }
+}
